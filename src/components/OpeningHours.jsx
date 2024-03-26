4010fef57294e7e7d6150276f1bb4a5bb5f5a7aa
Mathias Fontain <mathias.fontain@vol.at>
2024-03-26 18:56:19 +0100

    style openinghours

--- src/components/OpeningHours.jsx
+++ src/components/OpeningHours.jsx
@@ -7,23 +7,27 @@ const OpeningHours = () => {
       <h2>Öffnungszeiten</h2>
       <p>Wir freuen uns auf deinen Besuch!</p>
       <div className="openinghours__details">
-        <div className="openinghours__store">
-          <img src={store1} alt="Store 1" className="openinghours__image" />
-          <p className="openinghours__place">Coffee Place #1</p>
-          <ul>
-            <li>Mo geschlossen</li>
-            <li>Di - Fr: 07:00 - 16:30</li>
-            <li>Sa - So: 12:00 - 17:30</li>
-          </ul>
+        <div className="openinghours__wrapper">
+          <div className="openinghours__store">
+            <img src={store1} alt="Store 1" className="openinghours__image" />
+            <p className="openinghours__place">Coffee Place #1</p>
+            <ul>
+              <li>Mo geschlossen</li>
+              <li>Di - Fr: 07:00 - 16:30</li>
+              <li>Sa - So: 12:00 - 17:30</li>
+            </ul>
+          </div>
         </div>
-        <div className="openinghours__store">
-          <img src={store2} alt="Store 2" className="openinghours__image" />
-          <p className="openinghours__place">Coffee Place #2</p>
-          <ul>
-            <li>Mo - Do: 06:30 - 18:00</li>
-            <li>Fr geschlossen</li>
-            <li>Sa - So: 13:00 - 18:00</li>
-          </ul>
+        <div className="openinghours__wrapper">
+          <div className="openinghours__store">
+            <img src={store2} alt="Store 2" className="openinghours__image" />
+            <p className="openinghours__place">Coffee Place #2</p>
+            <ul>
+              <li>Mo - Do: 06:30 - 18:00</li>
+              <li>Fr geschlossen</li>
+              <li>Sa - So: 13:00 - 18:00</li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
