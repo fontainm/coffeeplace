163a8839dc9b2a5cb0c0a1e7103d0405192668e4
Mathias Fontain <mathias.fontain@vol.at>
2024-03-26 13:12:43 +0100

    improve header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import header from '/header.jpg';
+import logoWhite from '/logo_white.png';
 
 const Header = () => {
   return (
@@ -6,8 +7,9 @@ const Header = () => {
       className="section header"
       style={{ backgroundImage: `url(${header})` }}
     >
-      <h1 className="header__title">Coffee Place</h1>
-      <p className="header__subtitle">Your Coffee Place for coffee needs</p>
+      <img src={logoWhite} alt="Coffe Place Logo" className="header__title" />
+      <h1 style={{ display: 'none' }}>Coffee Place</h1>
+      <p className="header__subtitle">Dein bester Platz für den besten Kaffee</p>
     </div>
   );
 };
