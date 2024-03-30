42562a36bb021e84635fc7c9104df4870a7d95c7
Mathias Fontain <mathias.fontain@vol.at>
2024-03-30 07:59:08 +0100

    stores as json

--- src/components/OpeningHours.jsx
+++ src/components/OpeningHours.jsx
@@ -1,5 +1,4 @@
-import store1 from '/stores/store1.jpg';
-import store2 from '/stores/store2.jpg';
+import stores from '../data/stores.json';
 
 const OpeningHours = () => {
   return (
@@ -7,28 +6,23 @@ const OpeningHours = () => {
       <h2>Öffnungszeiten</h2>
       <p>Wir freuen uns auf deinen Besuch!</p>
       <div className="openinghours__details">
-        <div className="openinghours__wrapper">
-          <div className="openinghours__store">
-            <img src={store1} alt="Store 1" className="openinghours__image" />
-            <p className="openinghours__place">Coffee Place #1</p>
-            <ul>
-              <li>Mo geschlossen</li>
-              <li>Di - Fr: 07:00 - 16:30</li>
-              <li>Sa - So: 12:00 - 17:30</li>
-            </ul>
+        {stores.map((store) => (
+          <div key={store.id} className="openinghours__wrapper">
+            <div className="openinghours__store">
+              <img
+                src={`/stores/${store.image}`}
+                alt="Store 1"
+                className="openinghours__image"
+              />
+              <p className="openinghours__place">{store.name}</p>
+              <ul>
+                {store.openinghours.map((openinghour, index) => (
+                  <li key={index}>{openinghour}</li>
+                ))}
+              </ul>
+            </div>
           </div>
-        </div>
-        <div className="openinghours__wrapper">
-          <div className="openinghours__store">
-            <img src={store2} alt="Store 2" className="openinghours__image" />
-            <p className="openinghours__place">Coffee Place #2</p>
-            <ul>
-              <li>Mo - Do: 06:30 - 18:00</li>
-              <li>Fr geschlossen</li>
-              <li>Sa - So: 13:00 - 18:00</li>
-            </ul>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
